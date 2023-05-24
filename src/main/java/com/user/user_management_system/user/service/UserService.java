8c993667e7cfe58b2a78434f9b9b4d04d6c78f01
ngaboleo <ngaboleonard02@gmail.com>
2023-05-24 14:19:39 +0200

    expiration time on token on changing password

--- src/main/java/com/user/user_management_system/user/service/UserService.java
+++ src/main/java/com/user/user_management_system/user/service/UserService.java
@@ -263,16 +263,23 @@ public class UserService implements IUserService{
         try {
             Optional<ResetLinker> resetLinkerOptional = iResetLinkerRepository.findResetLinkerByTokenIgnoreCase(token);
             Date createAt = resetLinkerOptional.get().getCreatedAt();
+            System.out.println(createAt);
             Date expirationForToken = Date.from(createAt.toInstant().plus(Duration.ofMinutes(expirationDate)));
+            System.out.println(expirationForToken);
             LocalDateTime currentDateTime = LocalDateTime.now();
+            System.out.println(currentDateTime);
             Date currentTime = Date.from(currentDateTime.atZone(ZoneId.systemDefault()).toInstant());
-            if (resetLinkerOptional.isPresent() && expirationForToken.before(currentTime)){
-                Optional<User> userOptional = iUserRepository.findUserById(resetLinkerOptional.get().getUser().getId());
-                User user = userOptional.get();
-                user.setPassword(passwordEncoder.encode(newPassword));
-                iUserRepository.save(user);
-                iEmailSenderService.sendEmail(user.getEmail(), "Password Reset successfully", "Dear " +user.getFullName()+", your password has been reset successfully");
-                return new ResponseObject(user);
+            if (resetLinkerOptional.isPresent()){
+                if (!expirationForToken.before(currentTime)){
+                    Optional<User> userOptional = iUserRepository.findUserById(resetLinkerOptional.get().getUser().getId());
+                    User user = userOptional.get();
+                    user.setPassword(passwordEncoder.encode(newPassword));
+                    iUserRepository.save(user);
+                    iEmailSenderService.sendEmail(user.getEmail(), "Password Reset successfully", "Dear " +user.getFullName()+", your password has been reset successfully");
+                    return new ResponseObject(user);
+                }else {
+                    throw new HandleException(IMessageService.TOKEN_NOT_VALID);
+                }
             }else {
                 throw new HandleException(IMessageService.TOKEN_NOT_VALID);
             }
